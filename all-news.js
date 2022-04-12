96613b66357942b5b49bffbc3f8d8399d0218abf
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2022-04-12 16:13:52 -0700

    recent news added

--- all-news.js
+++ all-news.js
@@ -1,6 +1,9 @@
 document.write('\
 <ul>\
 <li>\
+March \'22 - Excited to be selected as a <a href="https://cloud.google.com/edu/researchers/innovators">Google Cloud Research Innovator</a>.\
+</li>\
+<li>\
 February \'22 - It was a pleasure to give <a href="https://wss.ce.sharif.edu/schedule">an invited talk</a> at my alma mater, Sharif University of Technology.\
 </li>\
 <li>\
--- recent-news.js
+++ recent-news.js
@@ -1,6 +1,9 @@
 document.write('\
 <ul>\
 <li>\
+March \'22 - Excited to be selected as a <a href="https://cloud.google.com/edu/researchers/innovators">Google Cloud Research Innovator</a>.\
+</li>\
+<li>\
 February \'22 - It was a pleasure to give <a href="https://wss.ce.sharif.edu/schedule">an invited talk</a> at my alma mater, Sharif University of Technology.\
 </li>\
 <li>\
@@ -11,9 +14,6 @@ August \'21 - Two papers accepted to <a href="https://acmsocc.org/2021/index.htm
 </li>\
 <li>\
 July \'21 - Visited University of Waterloo virtually to give <a href="https://cs.uwaterloo.ca/events/systems-and-networking-insights-from-two-serverless-characterization-studies">an invited talk at the Systems and Networking Seminar</a>.\
-</li>\
-<li>\
-May \'21 - Honored to give a keynote at the <a href="https://www.dagstuhl.de/en/program/calendar/semhp/?semnr=21201">Dagstuhl Seminar on Serverless Computing</a>!\
 </li>\
 </ul>\
 ');
