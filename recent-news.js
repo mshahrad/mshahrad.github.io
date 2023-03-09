5d80c5a4baae62066b312fdb366d2a2e185bc899
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2023-03-08 18:12:23 -0800

    fixed a few link redirections

--- recent-news.js
+++ recent-news.js
@@ -4,13 +4,13 @@ document.write('\
 November \'22 - Excited to be selected for the <a href="https://greencollege.ubc.ca/leading-scholars-program" target="_blank">2022-24 Cohort of the UBC Green College Leading Scholars</a>.\
 </li>\
 <li>\
-September \'22 - Participated and presented at the Serverless Computing roundtable at University of Waterloo. (related papers: <a href="https://arxiv.org/pdf/2206.13444.pdf" target="_blank">resource-centric serverless</a> and <a href="https://ubc-cirrus-lab.github.io/papers/socc21_multi-cloud_serverless.pdf" target="_blank">multi-cloud serverless</a>)\
+September \'22 - Participated and presented at the Serverless Computing roundtable at University of Waterloo. (related papers: <a href="https://arxiv.org/pdf/2206.13444.pdf" target="_blank">resource-centric serverless</a> and <a href="https://cirrus.ece.ubc.ca/papers/socc21_multi-cloud_serverless.pdf" target="_blank">multi-cloud serverless</a>)\
 </li>\
 <li>\
 June \'22 - Prof. Aastha Mehta and I are awarded a <a href="https://science.ubc.ca/research/stair" target="_blank">UBC Science STAIR</a> grant to pursue our interdisciplinary research.\
 </li>\
 <li>\
-May \'22 - Gave an invited talk on Multi-Cloud Serverless Computing at the IBM Global Technology Outlook seminar series on Future of Cloud Computing. (<a href="https://ubc-cirrus-lab.github.io/papers/socc21_multi-cloud_serverless.pdf" target="_blank">related paper</a>)\
+May \'22 - Gave an invited talk on Multi-Cloud Serverless Computing at the IBM Global Technology Outlook seminar series on Future of Cloud Computing. (<a href="https://cirrus.ece.ubc.ca/papers/socc21_multi-cloud_serverless.pdf" target="_blank">related paper</a>)\
 </li>\
 <li>\
 March \'22 - Excited to be selected as a <a href="https://cloud.google.com/edu/researchers/innovators" target="_blank">Google Cloud Research Innovator</a>.\
