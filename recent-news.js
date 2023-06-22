0b983ca65d747f7e3095b2a35270652dd9f8c80b
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2023-06-22 10:48:54 -0700

    updated the news

--- recent-news.js
+++ recent-news.js
@@ -1,7 +1,10 @@
 document.write('\
 <ul>\
 <li>\
-April \'22 - Two of our papers are accepted to <a href="https://www.usenix.org/conference/atc23" target="_blank">USENIX ATC \'23</a>!\
+June \'23 - Gave an invited talk at the <a href="https://www.serverlesscomputing.org/woscx2/" target="_blank">Second International Workshop on Serverless Computing Experience (WOSCx2)</a>. [<a href="https://www.youtube.com/watch?v=rAuBlRF7vRU" target="_blank">Talk Recording</a>] (related papers: <a href="https://cirrus.ece.ubc.ca/papers/socc21_multi-cloud_serverless.pdf" target="_blank">multi-cloud serverless</a> and <a href="https://cirrus.ece.ubc.ca/papers/atc23_unfaasener.pdf" target="_blank">UnFaaSener</a>)\
+</li>\
+<li>\
+April \'23 - Two of our papers are accepted to <a href="https://www.usenix.org/conference/atc23" target="_blank">USENIX ATC \'23</a>!\
 </li>\
 <li>\
 November \'22 - Excited to be selected for the <a href="https://greencollege.ubc.ca/leading-scholars-program" target="_blank">2022-24 Cohort of the UBC Green College Leading Scholars</a>.\
@@ -11,9 +14,6 @@ September \'22 - Participated and presented at the Serverless Computing roundtab
 </li>\
 <li>\
 June \'22 - Prof. Aastha Mehta and I are awarded a <a href="https://science.ubc.ca/research/stair" target="_blank">UBC Science STAIR</a> grant to pursue our interdisciplinary research.\
-</li>\
-<li>\
-May \'22 - Gave an invited talk on Multi-Cloud Serverless Computing at the IBM Global Technology Outlook seminar series on Future of Cloud Computing. (<a href="https://cirrus.ece.ubc.ca/papers/socc21_multi-cloud_serverless.pdf" target="_blank">related paper</a>)\
 </li>\
 </ul>\
 ');
