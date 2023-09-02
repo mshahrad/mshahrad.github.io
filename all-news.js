4e078363f0d26599b35200a99b38f6b13d7a12e0
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2023-09-01 22:28:32 -0700

    new paper added

--- all-news.js
+++ all-news.js
@@ -1,6 +1,9 @@
 document.write('\
 <ul>\
 <li>\
+September \'23 - Our paper is accepted to <a href="https://acmsocc.org/2023/" target="_blank">ACM SoCC \'23</a>.\
+</li>\
+<li>\
 June \'23 - Gave an invited talk at the <a href="https://www.serverlesscomputing.org/woscx2/" target="_blank">Second International Workshop on Serverless Computing Experience (WOSCx2)</a>. [<a href="https://www.youtube.com/watch?v=rAuBlRF7vRU" target="_blank">Talk Recording</a>] (related papers: <a href="https://cirrus.ece.ubc.ca/papers/socc21_multi-cloud_serverless.pdf" target="_blank">multi-cloud serverless</a> and <a href="https://www.usenix.org/system/files/atc23-sadeghian.pdf" target="_blank">UnFaaSener</a>)\
 </li>\
 <li>\
--- recent-news.js
+++ recent-news.js
@@ -1,6 +1,9 @@
 document.write('\
 <ul>\
 <li>\
+September \'23 - Our paper is accepted to <a href="https://acmsocc.org/2023/" target="_blank">ACM SoCC \'23</a>.\
+</li>\
+<li>\
 June \'23 - Gave an invited talk at the <a href="https://www.serverlesscomputing.org/woscx2/" target="_blank">Second International Workshop on Serverless Computing Experience (WOSCx2)</a>. [<a href="https://www.youtube.com/watch?v=rAuBlRF7vRU" target="_blank">Talk Recording</a>] (related papers: <a href="https://cirrus.ece.ubc.ca/papers/socc21_multi-cloud_serverless.pdf" target="_blank">multi-cloud serverless</a> and <a href="https://www.usenix.org/system/files/atc23-sadeghian.pdf" target="_blank">UnFaaSener</a>)\
 </li>\
 <li>\
@@ -11,9 +14,6 @@ November \'22 - Excited to be selected for the <a href="https://greencollege.ubc
 </li>\
 <li>\
 September \'22 - Participated and presented at the Serverless Computing roundtable at University of Waterloo. (related papers: <a href="https://arxiv.org/pdf/2206.13444.pdf" target="_blank">resource-centric serverless</a> and <a href="https://cirrus.ece.ubc.ca/papers/socc21_multi-cloud_serverless.pdf" target="_blank">multi-cloud serverless</a>)\
-</li>\
-<li>\
-June \'22 - Prof. Aastha Mehta and I are awarded a <a href="https://science.ubc.ca/research/stair" target="_blank">UBC Science STAIR</a> grant to pursue our interdisciplinary research.\
 </li>\
 </ul>\
 ');
