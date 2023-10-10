54e9c3e9df282ba51595d644b8aecab6e1dfc7f6
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2023-10-10 13:54:19 -0700

    news updates

--- all-news.js
+++ all-news.js
@@ -1,10 +1,13 @@
 document.write('\
 <ul>\
 <li>\
+October \'23 - Gave an invited talk on serverless at the MongoDB Perf Tech Talk Series.\
+</li>\
+<li>\
 September \'23 - Presented our collaborative research with the IBM Code Engine at the <a href="https://www.ibm.com/community/ibm-techxchange-conference/" target="_blank">IBM TechXchange Conference</a>.\
 </li>\
 <li>\
-September \'23 - Our paper is accepted to <a href="https://acmsocc.org/2023/" target="_blank">ACM SoCC \'23</a>.\
+September \'23 - Our paper is accepted to <a href="https://acmsocc.org/2023/" target="_blank">ACM SoCC \'23</a>. [<a href="https://cirrus.ece.ubc.ca/papers/socc23_parrotfish.pdf" target="_blank">Paper Draft</a>]\
 </li>\
 <li>\
 June \'23 - Gave an invited talk at the <a href="https://www.serverlesscomputing.org/woscx2/" target="_blank">Second International Workshop on Serverless Computing Experience (WOSCx2)</a>. [<a href="https://www.youtube.com/watch?v=rAuBlRF7vRU" target="_blank">Talk Recording</a>] (related papers: <a href="https://cirrus.ece.ubc.ca/papers/socc21_multi-cloud_serverless.pdf" target="_blank">multi-cloud serverless</a> and <a href="https://www.usenix.org/system/files/atc23-sadeghian.pdf" target="_blank">UnFaaSener</a>)\
