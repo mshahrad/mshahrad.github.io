950f6aa63a241ffefed1401f24c941505e68f27e
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2024-06-14 12:48:49 -0700

    recent news

--- all-news.js
+++ all-news.js
@@ -1,6 +1,12 @@
 document.write('\
 <ul>\
 <li>\
+June \'24 - Our paper is accepted to <a href="https://hotcarbon.org/" target="_blank">HotCarbon \'24</a>.\
+</li>\
+<li>\
+June \'24 - Our team will present the <a href="https://github.com/ubc-cirrus-lab/parrotfish" target="_blank">Parrotfish</a> tool at <a href="https://www.serverlesscomputing.org/woscx3/index.html" target="_blank">WOSCx3</a>.\
+</li>\
+<li>\
 April \'24 - Excited to contribute to the <a href="https://edas.info/web/nsf-wscs24/accepted-contributions.html" target="_blank">NSF Workshop on Sustainable Computing for Sustainability</a>.\
 </li>\
 <li>\
--- recent-news.js
+++ recent-news.js
@@ -1,6 +1,12 @@
 document.write('\
 <ul>\
 <li>\
+June \'24 - Our paper is accepted to <a href="https://hotcarbon.org/" target="_blank">HotCarbon \'24</a>.\
+</li>\
+<li>\
+June \'24 - Our team will present the <a href="https://github.com/ubc-cirrus-lab/parrotfish" target="_blank">Parrotfish</a> tool at <a href="https://www.serverlesscomputing.org/woscx3/index.html" target="_blank">WOSCx3</a>.\
+</li>\
+<li>\
 April \'24 - Excited to contribute to the <a href="https://edas.info/web/nsf-wscs24/accepted-contributions.html" target="_blank">NSF Workshop on Sustainable Computing for Sustainability</a>.\
 </li>\
 <li>\
@@ -8,12 +14,6 @@ October \'23 - Gave an invited talk on serverless at the MongoDB Perf Tech Talk 
 </li>\
 <li>\
 September \'23 - Gave a guest talk at the <a href="https://unikraft.org/hackathons/2023-09-vancouver" target="_blank">Vancouver Unikraft Hackathon</a>.\
-</li>\
-<li>\
-September \'23 - Presented our collaborative research with the IBM Code Engine at the <a href="https://www.ibm.com/community/ibm-techxchange-conference/" target="_blank">IBM TechXchange Conference</a>.\
-</li>\
-<li>\
-September \'23 - Our paper is accepted to <a href="https://acmsocc.org/2023/" target="_blank">ACM SoCC \'23</a>. [<a href="https://cirrus.ece.ubc.ca/papers/socc23_parrotfish.pdf" target="_blank">Paper Draft</a>]\
 </li>\
 </ul>\
 ');
