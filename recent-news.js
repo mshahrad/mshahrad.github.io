04d7a19c9cc9e658f641f564610dea5addb1470d
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2024-09-24 12:30:33 -0700

    caribou paper link added

--- recent-news.js
+++ recent-news.js
@@ -4,7 +4,7 @@ document.write('\
 September \'24 - Our paper is accepted to <a href="https://sp2025.ieee-security.org/" target="_blank">IEEE S&P (Oakland)</a>.\
 </li>\
 <li>\
-August \'24 - We have our paper accepted to <a href="https://sigops.org/s/conferences/sosp/2024/" target="_blank">SOSP \'24</a>.\
+August \'24 - We have our <a href="https://cirrus.ece.ubc.ca/papers/sosp24_caribou.pdf" target="_blank">paper</a> accepted to <a href="https://sigops.org/s/conferences/sosp/2024/" target="_blank">SOSP \'24</a>.\
 </li>\
 <li>\
 June \'24 - Our <a href="https://cirrus.ece.ubc.ca/papers/hotcarbon24_lin.pdf" target="_blank">paper</a> is accepted to <a href="https://hotcarbon.org/" target="_blank">HotCarbon \'24</a>.\
