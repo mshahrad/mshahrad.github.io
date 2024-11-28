f8fd4b53eddc50fbb84995940a3f76638f5c7992
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2024-11-28 10:22:41 -0800

    recent updates and fixed a broken link

--- recent-news.js
+++ recent-news.js
@@ -1,19 +1,19 @@
 document.write('\
 <ul>\
 <li>\
+November \'24 - Presented an invited talk at the <a href="https://events.vtools.ieee.org/m/435828" target="_blank">IEEE Vancouver Consultants Network\'s November Webinar</a>.\
+</li>\
+<li>\
+September \'24 - Our <a href="https://dl.acm.org/doi/10.1145/3698038.3698542" target="_blank">vision paper</a> with MIT and UMass researchers is accepted to SoCC \'24.\
+</li>\
+<li>\
 September \'24 - Our paper is accepted to <a href="https://sp2025.ieee-security.org/" target="_blank">IEEE S&P (Oakland)</a>.\
 </li>\
 <li>\
-August \'24 - We have our <a href="https://cirrus.ece.ubc.ca/papers/sosp24_caribou.pdf" target="_blank">paper</a> accepted to <a href="https://sigops.org/s/conferences/sosp/2024/" target="_blank">SOSP \'24</a>.\
+August \'24 - Our <a href="https://cirrus.ece.ubc.ca/papers/sosp24_caribou.pdf" target="_blank">paper</a> on Caribou is accepted to <a href="https://sigops.org/s/conferences/sosp/2024/" target="_blank">SOSP \'24</a>.\
 </li>\
 <li>\
 June \'24 - Our <a href="https://cirrus.ece.ubc.ca/papers/hotcarbon24_lin.pdf" target="_blank">paper</a> is accepted to <a href="https://hotcarbon.org/" target="_blank">HotCarbon \'24</a>.\
-</li>\
-<li>\
-June \'24 - Our team will present the <a href="https://github.com/ubc-cirrus-lab/parrotfish" target="_blank">Parrotfish</a> tool at <a href="https://www.serverlesscomputing.org/woscx3/index.html" target="_blank">WOSCx3</a>.\
-</li>\
-<li>\
-April \'24 - Excited to contribute to the <a href="https://edas.info/web/nsf-wscs24/accepted-contributions.html" target="_blank">NSF Workshop on Sustainable Computing for Sustainability</a>.\
 </li>\
 </ul>\
 ');
