836b32b7fd5d67a988645666d0ed51eb41beacc9
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2024-12-13 22:15:00 -0800

    recent updates

--- all-news.js
+++ all-news.js
@@ -1,7 +1,13 @@
 document.write('\
 <ul>\
 <li>\
-November \'24 - Presented an invited talk at the <a href="https://events.vtools.ieee.org/m/435828" target="_blank">IEEE Vancouver Consultants Network\'s November Webinar</a>.\
+December \'24 - Presented our latest work at the Azure Research - Systems group.\
+</li>\
+<li>\
+December \'24 - It was great presenting at the <a href="https://events.vtools.ieee.org/m/435828" target="_blank">vHive Community Meetup</a>.\
+</li>\
+<li>\
+November \'24 - Presented an invited talk at the <a href="https://events.vtools.ieee.org/m/435828" target="_blank">IEEE Vancouver Consultants Network</a>.\
 </li>\
 <li>\
 September \'24 - Our <a href="https://dl.acm.org/doi/10.1145/3698038.3698542" target="_blank">vision paper</a> with MIT and UMass researchers is accepted to SoCC \'24.\
