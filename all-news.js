a8ab00624e407f31e9a321165b4b83c6c5062466
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2026-05-10 12:29:23 -0700

    news section updated

--- all-news.js
+++ all-news.js
@@ -1,10 +1,13 @@
 document.write('\
 <ul>\
 <li>\
+April \'26 - Gave a <a href="https://cirrus.ece.ubc.ca/talks/26_sesame_keynote_shahrad.pdf" target="_blank">keynote</a> at the 4th SESAME workshop at EuroSys \'26.\
+</li>\
+<li>\
 December \'25 - Presented our latest work at Princeton University and Stevens Institute of Technology.\
 </li>\
 <li>\
-December \'25 - Our paper is accepted at USENIX NSDI \'26.\
+December \'25 - Our <a href="https://www.usenix.org/conference/nsdi26/presentation/baqershahi" target="_blank">paper</a> is accepted at USENIX NSDI \'26.\
 </li>\
 <li>\
 August \'25 - Two of our papers are accepted at ACM EuroSys \'26.\
--- recent-news.js
+++ recent-news.js
@@ -1,19 +1,19 @@
 document.write('\
 <ul>\
 <li>\
+April \'26 - Gave a <a href="https://cirrus.ece.ubc.ca/talks/26_sesame_keynote_shahrad.pdf" target="_blank">keynote</a> at the 4th SESAME workshop at EuroSys \'26.\
+</li>\
+<li>\
 December \'25 - Presented our latest work at Princeton University and Stevens Institute of Technology.\
 </li>\
 <li>\
-December \'25 - Our paper is accepted at USENIX NSDI \'26.\
+December \'25 - Our <a href="https://www.usenix.org/conference/nsdi26/presentation/baqershahi" target="_blank">paper</a> is accepted at USENIX NSDI \'26.\
 </li>\
 <li>\
 August \'25 - Two of our papers are accepted at ACM EuroSys \'26.\
 </li>\
 <li>\
 December \'24 - Presented our latest work at the Azure Research - Systems group.\
-</li>\
-<li>\
-December \'24 - It was great presenting at the <a href="https://events.vtools.ieee.org/m/435828" target="_blank">vHive Community Meetup</a>.\
 </li>\
 </ul>\
 ');
