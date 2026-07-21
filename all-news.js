f275f023150078e79b63eefe50e894e664b819e4
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2026-07-21 13:43:25 -0700

    recent updates

--- all-news.js
+++ all-news.js
@@ -1,6 +1,18 @@
 document.write('\
 <ul>\
 <li>\
+July \'26 - Served on the HotCarbon \'26 panel on "AI and Carbon".\
+</li>\
+<li>\
+July \'26 - Our collaborative work with UCSC and Indiana University was presented at HotCarbon \'26.\
+</li>\
+<li>\
+June \'26 - Zihan presented our paper at ACM e-Energy \'26 at Banff.\
+</li>\
+<li>\
+April \'26 - Proud of my students Changyuan and Nima for presenting two papers at EuroSys \'26.\
+</li>\
+<li>\
 April \'26 - Gave a <a href="https://cirrus.ece.ubc.ca/talks/26_sesame_keynote_shahrad.pdf" target="_blank">keynote</a> at the 4th SESAME workshop at EuroSys \'26.\
 </li>\
 <li>\
--- recent-news.js
+++ recent-news.js
@@ -1,19 +1,19 @@
 document.write('\
 <ul>\
 <li>\
+July \'26 - Served on the HotCarbon \'26 panel on "AI and Carbon".\
+</li>\
+<li>\
+July \'26 - Our collaborative work with UCSC and Indiana University was presented at HotCarbon \'26.\
+</li>\
+<li>\
+June \'26 - Zihan presented our paper at ACM e-Energy \'26 at Banff.\
+</li>\
+<li>\
+April \'26 - Proud of my students Changyuan and Nima for presenting two papers at EuroSys \'26.\
+</li>\
+<li>\
 April \'26 - Gave a <a href="https://cirrus.ece.ubc.ca/talks/26_sesame_keynote_shahrad.pdf" target="_blank">keynote</a> at the 4th SESAME workshop at EuroSys \'26.\
-</li>\
-<li>\
-December \'25 - Presented our latest work at Princeton University and Stevens Institute of Technology.\
-</li>\
-<li>\
-December \'25 - Our <a href="https://www.usenix.org/conference/nsdi26/presentation/baqershahi" target="_blank">paper</a> is accepted at USENIX NSDI \'26.\
-</li>\
-<li>\
-August \'25 - Two of our papers are accepted at ACM EuroSys \'26.\
-</li>\
-<li>\
-December \'24 - Presented our latest work at the Azure Research - Systems group.\
 </li>\
 </ul>\
 ');
